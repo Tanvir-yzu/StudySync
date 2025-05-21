ab95d896b85f0c244c59c31d9888b30440609c99
Tanvir-yzu <2020tanvir1971@gmail.com>
2025-05-22 02:53:28 +0800

    npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    host: '127.0.0.1', // or use true to expose to LAN
+    port: 5173,
+    strictPort: true, // fail if the port is in use
+    open: true,       // open browser automatically
   },
 });
